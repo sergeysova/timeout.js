75c95eedc11f19bec905ae69af7a0f11a9598b18
ssova-4xxi <ss@4xxi.com>
2016-07-27 12:58:39 +0300

    Refactor: remove `Promise.defer` call

--- src/timeout.js
+++ src/timeout.js
@@ -2,26 +2,29 @@
 
 class Timeout {
   constructor(ms, rs) {
-    this._pm = Promise.defer();
-    this._tm = setTimeout(() => this._pm.resolve(rs), ms);
+    this._pm = new Promise(function(resolve, reject) {
+      this._res = resolve;
+      this._rej = reject;
+    }.bind(this));
+    this._tm = setTimeout(() => this._res(rs), ms);
   }
 
   break() {
     clearTimeout(this._tm);
-    this._pm.reject();
+    this._rej();
     return this;
   }
 
   then(func, ctch) {
-    return this._pm.promise.then(func, ctch);
+    return this._pm.then(func, ctch);
   }
 
   catch(func) {
-    return this._pm.promise.catch(func);
+    return this._pm.catch(func);
   }
 
   chain(func) {
-    return this._pm.promise.chain(func);
+    return this._pm.chain(func);
   }
 }
 
